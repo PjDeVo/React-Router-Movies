fee97f05723ac9e7ebfb7332b3c5129890073be6
Owner <owner@Users-MacBook-Pro.local>
2020-02-16 23:20:15 -0500

    fixed key error on map from SavedList

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -4,8 +4,8 @@ import { NavLink, Link } from "react-router-dom";
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <Link to={"/movies/" + movie.id}>
+    {props.list.map((movie, index) => (
+      <Link key={index} to={"/movies/" + movie.id}>
         <span className="saved-movie">{movie.title}</span>
       </Link>
     ))}
