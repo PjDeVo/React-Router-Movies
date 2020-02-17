ab4c1eeae3b8ba9edf34ac6b8942f2c5d111ec8b
Owner <owner@Users-MacBook-Pro.local>
2020-02-16 23:01:18 -0500

    list of movies rendered. Links to seperate movie pages working on seperate cards.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Router>
+        <Route exact path="/" component={MovieList} />
+        <Route
+          path="/movies/:id"
+          render={props => {
+            const id = Number(props.match.params.id);
+            console.log("here is the id", id);
+            console.log("movies props", props);
+
+            return <Movie id={id} />;
+          }}
+        />
+      </Router>
     </div>
   );
 };
